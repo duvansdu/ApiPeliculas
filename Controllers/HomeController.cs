ddd5482db8d14656003757d6e4a680f7b52e3e47
Duvan Leal <yosoysdu@gmail.com>
2023-04-09 22:08:33 -0500

    commit proyecto Api Peliculas

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using aplicacionPel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace aplicacionPel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index(string pelicula)
+        {
+            var peliculas = await Peliculas.GetReservationAsync(pelicula);
+            return View(Enumerable.Repeat(peliculas, 1));
+        }
+
+    }
+    
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aplicacionPel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
